24e6918497f487e5b4c93255946400b7cb3ccf62
Alessandro Portale <alessandro@casaportale.de>
2007-12-01 10:56:13 +0000

    Proceeded with web page

--- webpage/images/resizeScreenshots.cmd
+++ webpage/images/resizeScreenshots.cmd
@@ -1,5 +1,5 @@
 for %%i in (screenshot_*.png) do ^
-convert -filter Hermite -resize 120x120 -colors 128 +dither %%i -depth 8 ..\%%~ni_thumb.png && ^
+convert -filter Hermite -resize 100x100 -colors 128 +dither %%i -depth 8 ..\%%~ni_thumb.png && ^
 pngout ..\%%~ni_thumb.png && ^
 convert -filter Hermite -resize 600x600 -colors 256 +dither %%i -depth 8 ..\%%i && ^
 pngout ..\%%i
--- webpage/index.php
+++ webpage/index.php
@@ -33,7 +33,7 @@
 	
 	function screenShot($imageBaseName, $imageCaption)
 	{
-		return "<a href=\"screenshot_$imageBaseName.png\"><img src=\"screenshot_".$imageBaseName."_thumb.png\" width=\"120\" height=\"96\" alt=\"$imageCaption\" /><br />$imageCaption</a>\n";
+		return "<a href=\"screenshot_$imageBaseName.png\"><img src=\"screenshot_".$imageBaseName."_thumb.png\" width=\"100\" height=\"80\" alt=\"$imageCaption\" /><br />$imageCaption</a>\n";
 	}
 	
 	function writeContent()
@@ -78,8 +78,8 @@
 			default:
 ?>
 				<h2><?= text('About', '&Uuml;ber'); ?></h2>
-				<?= text('<p>An educative software for physically challenged children. It contains ten learning modules covering "reading", "spelling", "counting", "addition", "subtraction", "reading notes", "determining tone intervalls", "playing piano keyboard" and "reading clocks". The program is especially suited for physically handicapped children, because it is also possible to use it completely with a touch screen. The elements of the user interface are big and clear.</p><p>The software was created during a practical semestre. Since a few years, the <a href="http://www.luckenberger-schule.de/">Luckenberger elementary school</a> in <a href="http://www.stadt-brb.de/stadt-brandenburg/">Brandenburg an der Havel</a> integrates physically challenged children into the lessons. This software was concepted together with Heiko Nachtigall, the vice president of the school.</p><p>When a physically challenged child has problems using pen and paper or other pedagogical material, it may happen that th child stays behind other children with its learning pensum. Using the hands when is are very essential for learning at the age of 6 or 7. A learning program which is really easy to use with a touch screen and has big and uncomplicated user interface elements could maybe compensate those motorical problems a little bit.</p>',
-'<p>Ein Lernprogramm für körperlich behinderte Grundschulkinder. In zehn Lerneinheiten werden die Themen "Lesen", "Buchstabieren", "Zählen", "Addition", "Subtraktion", "Noten lesen", "Noten-Intervalle bestimmen", "Klavier spielen" und "Uhren Lesen" behandelt. Das Programm ist besonders für körperlich behinderte Kinder geeignet, da die Benutzung auch mit einem Touchscreen funktioniert. Die Bedienelemente sind groß und deutlich.</p><p>Das Programm enstand im Zuge eines Praktikumssemesters. Die <a href="http://www.luckenberger-schule.de/">Luckenberger Grundschule</a> in <a href="http://www.stadt-brb.de/stadt-brandenburg/">Brandenburg an der Havel</a> integriert seit einigen Jahren körperlich behinderte und lernbehinderte Kinder in den Schulalltag. Zusammen mit Herrn Heiko Nachtigall, dem stellvertretenden Schulleiter der Schule, wurde es konzipiert.</p><p>Ein körperlich behindertes Kind kann, wenn es Schwierigkeiten im Umgang mit Stift und Papier oder pädagogischem Spielzeug hat, in den frühen Grundschuljahren evtl. mit dem Lernpensum zurück bleiben. Das Be-"greifen" spielt beim Lernen eine wichtige Rolle. Ein leicht per Touchscreen bedienbares Lernprogramm mit großen Bedienelementen kann diese motorischen Schwierigkeiten eventuell teilweise kompensieren.</p>') ?>
+				<?= text('<p>An educative software for physically challenged children which is completely usable with a touch screen. The elements of the user interface are big and clear.</p><p>It was created during a practical semestre. Since a few years, the <a href="http://www.luckenberger-schule.de/">Luckenberger elementary school</a> in <a href="http://www.stadt-brb.de/stadt-brandenburg/">Brandenburg an der Havel</a> integrates physically challenged children into the lessons. This software was concepted together with Heiko Nachtigall, the vice president of the school.</p><p>When a physically challenged child has problems using pen and paper or other pedagogical material, it may happen that th child stays behind other children with its learning pensum. Using the hands when is are very essential for learning at the age of 6 or 7. A learning program which is really easy to use with a touch screen and has big and uncomplicated user interface elements could maybe compensate those motorical problems a little bit.</p>',
+'<p>Ein Lernprogramm für körperlich behinderte Grundschulkinder, das für die Benutzung mit einem Touchscreen geeignet ist. Die Bedienelemente sind groß und deutlich.</p><p>Das Programm enstand im Zuge eines Praktikumssemesters. Die <a href="http://www.luckenberger-schule.de/">Luckenberger Grundschule</a> in <a href="http://www.stadt-brb.de/stadt-brandenburg/">Brandenburg an der Havel</a> integriert seit einigen Jahren körperlich behinderte und lernbehinderte Kinder in den Schulalltag. Zusammen mit Herrn Heiko Nachtigall, dem stellvertretenden Schulleiter der Schule, wurde es konzipiert.</p><p>Ein körperlich behindertes Kind kann, wenn es Schwierigkeiten im Umgang mit Stift und Papier oder pädagogischem Spielzeug hat, in den frühen Grundschuljahren evtl. mit dem Lernpensum zurück bleiben. Das Be-"greifen" spielt beim Lernen eine wichtige Rolle. Ein leicht per Touchscreen bedienbares Lernprogramm mit großen Bedienelementen kann diese motorischen Schwierigkeiten eventuell teilweise kompensieren.</p>') ?>
 <?
 		};
 	}
@@ -106,17 +106,14 @@
 				<div id="title">Touch'n'learn</div><div id="tagline"><?= tagLine() ?></div>
 				<a href="<?=linkParameters("","english")?>"><img src="flagEnglish<?= text('_selected', ''); ?>.png" width="26" height="17" alt="English" /></a><a href="<?=linkParameters("","german")?>"><img src="flagGerman<?= text('', '_selected'); ?>.png" width="26" height="17" alt="Deutsch" /></a>
 			</div>
-			<div id="navigation">
-				<ul id="navigationlist"><li><a href="<?=linkParameters("about","")?>"<?=conditionalSelectedClass("about")?>><?= text('About', '&Uuml;ber'); ?></a></li><li><a href="<?=linkParameters("screenshots","")?>"<?=conditionalSelectedClass("screenshots")?>><?= text('Screenshots', 'Bildschirmfotos'); ?></a></li><li><a href="<?=linkParameters("license","")?>"<?=conditionalSelectedClass("license")?>><?= text('License', 'Lizenz'); ?></a></li></ul>
-				<a class="external" href="http://www.google.com"><?= text('Download', 'Herunterladen'); ?></a>
-				<a class="external" href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=209276&amp;type=1" width="88" height="31" alt="SF.net Logo" /></a>
-				<a class="external" href="http://sourceforge.net/projects/touchandlearn/"><?= text('SF project page', 'SF Projekt-Seite'); ?></a>
-			</div>
+			<ul id="navigationlist">
+				<li><a href="<?=linkParameters("about","")?>"<?=conditionalSelectedClass("about")?>><?= text('About', '&Uuml;ber'); ?></a></li><li><a href="<?=linkParameters("screenshots","")?>"<?=conditionalSelectedClass("screenshots")?>><?= text('Screenshots', 'Bildschirmfotos'); ?></a></li><li><a class="external" href="http://www.google.com"><?= text('Download', 'Herunterladen'); ?></a></li><li><a href="<?=linkParameters("license","")?>"<?=conditionalSelectedClass("license")?>><?= text('License', 'Lizenz'); ?></a></li><li><a class="external" href="http://sourceforge.net/projects/touchandlearn/"><?= text('SF project page', 'SF Projekt-Seite'); ?></a></li></ul>
 			<div id="content">
 <?=writeContent();?>
 
 			</div>
 			<div id="footer">
+				<a class="external" href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=209276&amp;type=1" width="88" height="31" alt="SF.net Logo" /></a>
 			</div>
 		</div>
 	</body>
